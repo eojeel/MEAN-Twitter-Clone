9f9bcb4c1e4680efe08b93cc4aadc35d68a19a92
Joe Lee <grandmstramrod@gmail.com>
2015-10-06 21:27:09 +0100

    finised API, Get, Post

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var Post = require('./models/post')
 
 var app = express()
 app.use(bodyParser.json())
 
-var Post = require('./models/post')
+app.get('/api/posts', function(req, res, next) {
+  Post.find(function(err, post) {
+    if (err) {
+      return next(err)
+    }
+    res.json(post)
+  })
+})
+
 app.post('/api/posts', function(req, res, next) {
-  var post = new post({
+  var post = new Post({
     username: req.body.username,
     body: req.body.body
   })
@@ -16,13 +25,6 @@ app.post('/api/posts', function(req, res, next) {
     }
     res.json(201, post)
   })
-})
-
-app.get('/api/posts', function(req, res) {
-  res.json([{
-    username: 'dickeyxxx',
-    body: 'node rock!'
-  }])
 })
 
 app.listen(3000, function() {
